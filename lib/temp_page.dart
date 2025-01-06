394991bd83a261096a973c431b82bb7adda365ae
Sundram Kumar <sundramroy1208@gmail.com>
2025-01-06 20:02:53 +0530

    Made API functional

--- lib/temp_page.dart
+++ lib/temp_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TempPage extends StatefulWidget {
   const TempPage({super.key});
