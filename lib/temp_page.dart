af662db39021f662b3f3b444fe62b6b73ca35c30
Sundram Kumar <sundramroy1208@gmail.com>
2025-01-06 13:16:38 +0530

    Custom Button class

--- lib/temp_page.dart
+++ lib/temp_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 // import 'packet:flutter/material.dart';
 
 class TempPage extends StatefulWidget {
@@ -6,6 +7,38 @@ class TempPage extends StatefulWidget {
 
   @override
   State<TempPage> createState() => _TempPageState();
+}
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final Color color;
+  const CustomButton({
+    required this.label,
+    required this.onPressed,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          foregroundColor: Colors.white,
+          elevation: 10,
+          minimumSize: Size(double.infinity, 50),
+        ),
+        child: Text(label),
+      ),
+    );
+  }
 }
 
 class _TempPageState extends State<TempPage> {
@@ -97,49 +130,19 @@ class _TempPageState extends State<TempPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Expanded(
-                    child: Container(
-                      margin:
-                          const EdgeInsets.only(top: 20, left: 10, bottom: 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          fetchAmt();
-                        },
-                        style: TextButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 27, 95, 151),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          foregroundColor: Colors.white,
-                          elevation: 10,
-                          minimumSize: const Size(double.infinity, 50),
-                        ),
-                        child: const Text('CONVERT'),
-                      ),
+                    child: CustomButton(
+                      label: "CONVERT",
+                      onPressed: fetchAmt,
+                      color: const Color.fromARGB(255, 27, 95, 151),
                     ),
                   ),
                   const SizedBox(width: 10), // Add spacing between the buttons
                   Expanded(
-                    child: Container(
-                      margin:
-                          const EdgeInsets.only(top: 20, right: 10, bottom: 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          reset();
-                        },
-                        style: TextButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 151, 52, 27),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          foregroundColor: Colors.white,
-                          elevation: 10,
-                          minimumSize: const Size(double.infinity, 50),
-                        ),
-                        child: const Text('RESET'),
-                      ),
-                    ),
+                    child: CustomButton(
+                      label: "RESET",
+                      onPressed: reset,
+                      color: const Color.fromARGB(255, 238, 33, 33),
+                    )
                   ),
                 ],
               )
